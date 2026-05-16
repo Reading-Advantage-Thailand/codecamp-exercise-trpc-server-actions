a54b69c02069a671278df1ce27067ef945051ac5
Daniel Bo <bodangren@gmail.com>
2026-05-16 08:39:22 +0800

    add reference solution for tRPC & domain functions exercise

--- src/domain/todos.ts
+++ src/domain/todos.ts
@@ -3,22 +3,20 @@ import type * as store from "../store.js";
 type DB = typeof store;
 
 export function listTodos({ db }: { db: DB }) {
-  // TODO: Return all todos from the store
-  throw new Error("Not implemented");
+  return db.getAll();
 }
 
 export function createTodo({ db, input }: { db: DB; input: { title: string } }) {
-  // TODO: Add a new todo using db.add() and return it
-  throw new Error("Not implemented");
+  return db.add(input.title);
 }
 
 export function toggleTodo({ db, input }: { db: DB; input: { id: string } }) {
-  // TODO: Get the todo by ID, flip its completed status, return it
-  // Throw an error if the todo is not found
-  throw new Error("Not implemented");
+  const todo = db.getById(input.id);
+  if (!todo) throw new Error("Todo not found");
+  return db.update(input.id, { completed: !todo.completed })!;
 }
 
 export function deleteTodo({ db, input }: { db: DB; input: { id: string } }) {
-  // TODO: Remove the todo by ID, return { success: true/false }
-  throw new Error("Not implemented");
+  const success = db.remove(input.id);
+  return { success };
 }
